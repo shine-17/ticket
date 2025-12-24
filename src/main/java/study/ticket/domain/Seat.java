c15a1bf22f76e401964bffe7d916e3997cf8ba9a
dohun <shine_17@naver.com>
2025-12-24 23:07:24 +0900

    refactor: update domain

--- src/main/java/study/ticket/domain/Seat.java
+++ src/main/java/study/ticket/domain/Seat.java
@@ -31,5 +31,25 @@ public class Seat {
     @Positive
     private int number;
 
-//    private boolean booked;
+    /*
+        A(Available 예약 가능)
+                H(Held 결제 대기중)
+                O(On Hold 임시 선점 중)
+        P(Preempt 선점 중)
+        S(Sold,Occupied 판매 완료, 예약 확정)
+        B(Blocked,Reserved 판매 불가)
+        R(Refunded/Returned 환불된 좌석, 재판매 가능 여부 결정 전 임시 상태)
+     */
+    private char state;
+
+    private long show_id;
+
+    public boolean available() {
+        return state == 'A';
+    }
+
+    public void preempt() {
+        state = 'P';
+    }
+
 }
