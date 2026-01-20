383ba037663b6fdf549da09f7d9df6d54e168732
dohun <shine_17@naver.com>
2026-01-20 22:22:26 +0900

    feat: add field to booking domain

--- src/main/java/study/ticket/domain/Booking.java
+++ src/main/java/study/ticket/domain/Booking.java
@@ -21,26 +21,30 @@ public class Booking {
     private Member member;
 
     @OneToOne
+    @JoinColumn(name = "show_id", referencedColumnName = "id")
+    private Show show;
+
+    @OneToOne
     @JoinColumn(name = "seat_id", referencedColumnName = "id")
     private Seat seat;
 
-    public static Booking of(Member member, Seat seat) {
-        return new Booking(member, seat);
+    public static Booking of(Member member, Show show, Seat seat) {
+        return new Booking(member, show, seat);
     }
 
-    public static List<Booking> of(Member member, List<Seat> seats) {
+    public static List<Booking> of(Member member, Show show, List<Seat> seats) {
 //        if (seats.size() > 2) throw new IllegalStateException("1인 최대 2매까지 예매 가능합니다.");
-
         List<Booking> bookings = new ArrayList<>();
         for (Seat seat : seats) {
-            bookings.add(new Booking(member, seat));
+            bookings.add(new Booking(member, show, seat));
         }
 
         return bookings;
     }
 
-    private Booking(Member member, Seat seat) {
+    private Booking(Member member, Show show, Seat seat) {
         this.member = member;
+        this.show = show;
         this.seat = seat;
     }
 }
