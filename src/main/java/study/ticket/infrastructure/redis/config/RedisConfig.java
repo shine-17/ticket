77be1ba7e59093fb812a79dcf47a22e3d994e741
dohun <shine_17@naver.com>
2026-02-02 23:12:21 +0900

    refactor: redis config, key 분리

--- src/main/java/study/ticket/infrastructure/redis/config/RedisConfig.java
+++ src/main/java/study/ticket/infrastructure/redis/config/RedisConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import study.ticket.domain.Booking;
+import study.ticket.infrastructure.redis.dto.RedisBookingDto;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+    public RedisTemplate<String, Object> objectRedisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
 
@@ -21,6 +25,22 @@ public class RedisConfig {
         // Value: Plain string (따옴표 없이 저장)
         template.setValueSerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, RedisBookingDto> bookingRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, RedisBookingDto> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // Key: String
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        // Value: Plain string (따옴표 없이 저장)
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return template;
     }
--- src/main/java/study/ticket/infrastructure/redis/seat/RedisKeys.java
+++ src/main/java/study/ticket/infrastructure/redis/seat/RedisKeys.java
@@ -7,8 +7,9 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public enum RedisKeys {
-    BOOKED("show:%s:seat:booked:%s"),
-    PREEMPTED("show:%s:seat:preempted:%s");
+    BOOKED_SEAT("show:%s:seat:booked:%s"),
+    PREEMPTED_SEAT("show:%s:seat:preempted:%s"),
+    BOOKING("booked:%s");
 
     private final String format;
 
@@ -18,6 +19,10 @@ public enum RedisKeys {
 
     public String generateKey(Long showId, Long seatId) {
         return format.formatted(showId, seatId);
+    }
+
+    public String generateKey(Long bookingId) {
+        return format.formatted(bookingId);
     }
 
     public List<String> generateKeys(Long showId, List<Long> seatIds) {
