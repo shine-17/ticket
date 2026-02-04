ee50629ca152725bb90d4d965cf2cf12a155a0c5
dohun <shine_17@naver.com>
2026-02-04 16:51:53 +0900

    refactor: add serializer to field

--- src/main/java/study/ticket/domain/Show.java
+++ src/main/java/study/ticket/domain/Show.java
@@ -1,5 +1,10 @@
 package study.ticket.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -23,6 +28,9 @@ public class Show {
 
     private long venue_id;
 
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
 
     private int round;
