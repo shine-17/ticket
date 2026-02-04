ce575fb2b844a351ec23f2793e7ad687e9af38ba
dohun <shine_17@naver.com>
2026-02-04 18:18:16 +0900

    test: test 설정 파일 추가

--- src/test/java/study/ticket/TicketApplicationTests.java
+++ src/test/java/study/ticket/TicketApplicationTests.java
@@ -2,8 +2,10 @@ package study.ticket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TicketApplicationTests {
 
 	@Test
