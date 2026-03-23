ff85c4d98e02fbdcdbe646b2ce138cb6c053f44b
dohun <shine_17@naver.com>
2026-03-23 23:18:01 +0900

    feat: add filter (https)

--- src/main/java/study/ticket/interfaces/filter/HttpsFilter.java
+++ src/main/java/study/ticket/interfaces/filter/HttpsFilter.java
@@ -3,11 +3,13 @@ package study.ticket.interfaces.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class HttpsFilter implements Filter {
 
     @Override
@@ -17,6 +19,7 @@ public class HttpsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         if (!request.isSecure()) {
+            log.info("Request is not secure");
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "HTTPS Required");
             return;
         }
